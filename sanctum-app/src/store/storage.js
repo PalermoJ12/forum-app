cb1010efd464770593cc8fed1b2452aa38cbbf10
OmictinAyahel <omictinayahel@gmail.com>
2024-05-22 11:34:43 +0800

    Add Edit Comment Function

--- sanctum-app/src/store/storage.js
+++ sanctum-app/src/store/storage.js
@@ -6,11 +6,17 @@ const store = createStore({
   state() {
     return {
       posts: {},
+      comment: {},
       selectedPost: {},
+      selectedComment: {},
       myPost: {},
+      myComment: {},
       updatepost: {
         postTitle: "",
         postBody: "",
+      },
+      updatecomment: {
+        commmentbody: "",
       },
       user_id: localStorage.getItem("user"),
     };
@@ -21,6 +27,18 @@ const store = createStore({
     },
     setMyPost(state, data) {
       state.myPost = data;
+    },
+    setComment(state, comment) {
+      state.comment = comment;
+    },
+    setMyComment(state, data) {
+      state.myComment = data;
+    },
+    updateComment(state, updatedComment) {
+      const commentIndex = state.selectedPost.comments.findIndex(comment => comment.id === updatedComment.id);
+      if (commentIndex !== -1) {
+        state.selectedPost.comments.splice(commentIndex, 1, updatedComment);
+      }
     },
   },
   actions: {
@@ -41,7 +59,33 @@ const store = createStore({
         })
         .catch((err) => console.log(err));
     },
-  },
+    getComment({ commit }) {
+      axiosClient
+        .get("/comments")
+        .then((res) => {
+          console.log(res.data.posts);
+          commit("setComment", res.data.comment);
+        })
+        .catch((err) => console.log(err));
+    },
+    getMyComment({ commit }) {
+      axiosClient
+        .get("/mycomment")
+        .then((res) => {
+          commit("setMyComment", res.data.response);
+        })
+        .catch((err) => console.log(err));
+    },
+    updateComment({ commit }, { selectedPost, updatedComment }) {
+      return axiosClient.put(`/commentUpdate/${updatedComment.id}`, updatedComment)
+        .then((res) => {
+          commit("updateComment", { selectedPost, updatedComment: res.data.updatedComment });
+          return res;
+        }).catch((err) => {
+          console.log(err);
+          throw err;
+        });
+    },
+  }
 });
-
 export default store;
